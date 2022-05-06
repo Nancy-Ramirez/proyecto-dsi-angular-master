4c68e6133a14073a05747bbaba0ee61283c48f1b
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2022-05-05 20:53:19 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { FormEmpleadoComponent } from './empleado/form-empleado.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe';
+import { FormHabitacionComponent } from './habitacion/form-habitacion.component';
+import { HabitacionComponent } from './habitacion/habitacion.component';
+import { DepartamentoComponent } from './departamento/departamento.component';
+import { CargoComponent } from './cargo/cargo.component';
+import { FormDepartamentoComponent } from './departamento/form-departamento.component';
+import { FormCargoComponent } from './cargo/form-cargo.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BusCargoPipe } from './pipes/bus-cargo.pipe';
+import { BusHabitacionPipe } from './pipes/bus-habitacion.pipe';
+import { BusDepartamentoPipe } from './pipes/bus-departamento.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpleadoComponent,
+    FormEmpleadoComponent,
+    FilterPipe,
+    FormHabitacionComponent,
+    HabitacionComponent,
+    DepartamentoComponent,
+    CargoComponent,
+    FormDepartamentoComponent,
+    FormCargoComponent,
+    InicioComponent,
+    BusCargoPipe,
+    BusHabitacionPipe,
+    BusDepartamentoPipe,
+    BarraLateralComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/empleado/form-empleado.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Empleado } from './empleado';
+import { EmpleadoService } from './empleado.service';
+import { Cargo } from '../cargo/cargo';
+import { Departamento } from '../departamento/departamento';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CargoService } from '../cargo/cargo.service';
+
+@Component({
+  selector: 'app-form-empleado',
+  templateUrl: './form-empleado.component.html',
+  styleUrls: ['./form-empleado.component.css'],
+})
+export class FormEmpleadoComponent implements OnInit {
+  registroForm!: FormGroup;
+  submitted = false;
+
+  empleado: Empleado = new Empleado();
+  cargo?: Cargo[];
+  departamento?: Departamento[];
+
+  titulo: string = 'Registro de Empleado';
+
+  constructor(
+    private empleadoService: EmpleadoService,
+    private cargoService: CargoService,
+    private router: Router,
+    private activateRoute: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.cargar();
+    this.cargos();
+    this.departamentos();
+
+    this.registroForm = this.formBuilder.group({
+      nombres: ['', [Validators.required, Validators.maxLength(200)]],
+      apellidos: ['', [Validators.required, Validators.maxLength(200)]],
+      fecha_nacimiento: ['', Validators.required],
+      nacionalidad: ['', Validators.required],
+      dui: [
+        '',
+        [Validators.required, Validators.maxLength(9), Validators.minLength(9)],
+      ],
+      isss: [
+        '',
+        [Validators.required, Validators.maxLength(9), Validators.minLength(9)],
+      ],
+      nup: [
+        '',
+        [Validators.required, Validators.maxLength(9), Validators.minLength(9)],
+      ],
+      direccion: ['', Validators.required],
+      ciudad: ['', Validators.required],
+      telefono: ['', Validators.required],
+      sexo: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      fecha_contratacion: ['', Validators.required],
+      id_cargo: ['', Validators.required],
+      id_departamento: ['', Validators.required],
+    });
+  }
+  get form() {
+    return this.registroForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+  }
+  onReset() {
+    this.submitted = false;
+    this.registroForm.reset();
+  }
+  cargar(): void {
+    this.activateRoute.params.subscribe((e) => {
+      let id = e['id'];
+      if (id) {
+        this.empleadoService
+          .get(id)
+          .subscribe((es) => ((this.empleado = es), console.log(es)));
+      }
+    });
+  }
+
+  create(): void {
+    this.empleadoService
+      .create(this.empleado)
+      .subscribe((res) => this.router.navigate(['/empleados']));
+  }
+
+  update(): void {
+    this.empleadoService
+      .update(this.empleado)
+      .subscribe((res) => this.router.navigate(['/empleados']));
+  }
+
+  cargos(): void {
+    this.cargoService.getCargos().subscribe((c) => (this.cargo = c));
+  }
+
+  changeCargo(e: any) {
+    console.log(e.target.value);
+  }
+
+  departamentos(): void {
+    this.empleadoService
+      .getDepartamentos()
+      .subscribe((d) => (this.departamento = d));
+  }
+}

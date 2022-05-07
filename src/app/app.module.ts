121a21d96d9fc77ec70f9f2cfa25b39a5138c860
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2022-05-07 01:47:48 -0600

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { EmpleadoComponent } from './empleado/empleado.component';
 import { FormEmpleadoComponent } from './empleado/form-empleado.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -64,6 +65,7 @@ import { BusDepartamentoPipe } from './pipes/bus-departamento.pipe';
     MatButtonModule,
     MatGridListModule,
     MatInputModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

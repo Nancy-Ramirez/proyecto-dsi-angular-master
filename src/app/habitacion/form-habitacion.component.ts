7d8269ff0762035f4a1d792374e59ea731d0a23a
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2022-05-21 21:26:08 -0600

    para hacer mascaras

--- src/app/habitacion/form-habitacion.component.ts
+++ src/app/habitacion/form-habitacion.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Habitacion } from './habitacion';
-import { Empleado } from '../empleado/empleado';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HabitacionService } from './habitacion.service';
+import {FormBuilder,Validators} from '@angular/forms';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form-habitacion',
@@ -15,16 +16,38 @@ export class FormHabitacionComponent implements OnInit {
   habitacion: Habitacion = new Habitacion();
   titulo: string = 'Registro de Habitacion';
   submitted = false;
-
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+  
+  
   constructor(
     private activateRoute: ActivatedRoute,
     private router: Router,
-    private habitacionService: HabitacionService
+    private habitacionService: HabitacionService,
+    private _formBuilder: FormBuilder
   ) {}
 
+  
   ngOnInit(): void {
     this.cargar();
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: '',
+    });
+
+    this.registroForm = this._formBuilder.group({
+      numero_de_habitacion: ['', [Validators.required, Validators.maxLength(3), Validators.minLength(3)]],
+      numero_de_piso: ['', [Validators.required]],
+      tipo_habitacion: ['', Validators.required],
+      descripcion_habitacion: ['', Validators.required],
+      direccion: ['', [Validators.required]],
+      max_personas: ['', [Validators.required]],
+    });
   }
+
+  
 
   onSubmit(): void {
     this.submitted = true;
@@ -39,15 +62,42 @@ export class FormHabitacionComponent implements OnInit {
       }
     });
   }
-  create(): void {
+  create(): void 
+   {
+     swal
+  .fire({
+  position: 'center',
+  icon: 'success',
+  title: 'Registrado con éxito',
+  showConfirmButton: false,
+  timer: 1500
+  
+    })
     this.habitacionService
       .create(this.habitacion)
-      .subscribe((res) => this.router.navigate(['/habitaciones']));
+      .subscribe((res) => (this.router.navigate(['/habitaciones']), console.log(res))
+      );
   }
 
   update(): void {
-    this.habitacionService
+    swal.
+  fire({
+  title: '¿Desea guardar los cambios?',
+  showDenyButton: true,
+  showCancelButton: true,
+  confirmButtonText: 'Guardar',
+  denyButtonText: `No guardar`,
+}).then((result) => {
+  /* Read more about isConfirmed, isDenied below */
+  if (result.isConfirmed) {
+       this.habitacionService
       .update(this.habitacion)
       .subscribe((res) => this.router.navigate(['/habitaciones']));
+    swal.fire('Cambios guardados', '', 'success')
+  } else if (result.isDenied) {
+    swal.fire('Los cambios no han sido guardados', '', 'info')
+    this.router.navigate(['/habitaciones'])
   }
+});
+}
 }
